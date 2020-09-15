afe550bed3417c102279a4913e32afe0df3e1991
dacardenasa <diego.cardenasaleg@gmail.com>
2020-09-14 20:36:54 -0500

    Upload project files challenge social media

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const checkbox = document.querySelector("#input-theme");
+const title = document.querySelector("#theme-box-title");
+const html = document.querySelector("#html-content");
+
+checkbox.addEventListener("click", function(event){
+  if (checkbox.checked) {
+    title.innerHTML = "Light Mode";
+    html.setAttribute("data-theme", "dark");
+  } else {
+    title.innerHTML = "Dark Mode";
+    html.setAttribute("data-theme", "light");
+  }
+});
