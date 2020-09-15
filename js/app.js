99022123c4a44ff1c8368f3c233414f3848c74c7
dacardenasa <diego.cardenasaleg@gmail.com>
2020-09-15 09:56:43 -0500

    Update app.js checkbox.checked validation

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const title = document.querySelector("#theme-box-title");
 const html = document.querySelector("#html-content");
 
 checkbox.addEventListener("click", function(event){
-  if (checkbox.checked) {
+  if (!checkbox.checked) {
     title.innerHTML = "Light Mode";
     html.setAttribute("data-theme", "dark");
   } else {
